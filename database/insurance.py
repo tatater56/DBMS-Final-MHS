fd46cabaa340f83dabcc8d5b8849630bfa275f4a
Patrick Malachowski <malachowski.patrick@gmail.com>
2024-04-30 20:50:01 -0400

    Complete insurance functionality

---
+++ database/insurance.py
@@ -0,0 +1,26 @@
+import database.db as db
+
+from util import validate_int, is_empty_string
+
+def get_all():
+    print("insurance.get_all() called!")
+    return db.select_all('insurancecompany')
+
+def get(Ins_id):
+    print(f"insurance.get({Ins_id} called!)")
+    return db.select('insurancecompany', 'Ins_id', Ins_id)
+
+def create(insurance):
+    print(f"insurance.create({insurance}) called!")
+
+    if(not insurance or not isinstance(insurance, dict)):
+        return False
+    
+    return db.insert('insurancecompany', insurance)
+
+def update(insurance):
+    pass
+
+def delete(id):
+   return db.delete('insurancecompany', 'Ins_id', id)
+    
--- routes/employee_facility_routes.py
+++ routes/employee_facility_routes.py
@@ -1,9 +1,7 @@
 from flask import render_template, url_for, redirect, request
 from __main__ import app
 
-import database.employee as employee
-import database.facility as facility
-import database.db as db
+from database import db, employee, facility, insurance
 
 import util
 
@@ -20,7 +18,8 @@ def efm_page():
                             hcps = employee.get_all_hcps(),
                             facilities = facility.get_all(),
                             opses = facility.get_all_opses(),
-                            offices = facility.get_all_offices())
+                            offices = facility.get_all_offices(),
+                            insurances = insurance.get_all())
 
 
 # New employee
@@ -145,7 +144,6 @@ def api_new_facility():
     return redirect(url_for('efm_page', 
                         message=message))
 
-
 # Delete facility
 
 @app.route('/DeleteFacility/<FacID>')
@@ -168,6 +166,42 @@ def api_delete_facility(FacID):
         return redirect(url_for('efm_page',
                                 message=result))
 
-
 # TODO: Update facility
+
+# New insurance company
+
+@app.route('/NewInsurance')
+def new_insurance_page():
+    return render_template('insurance/new.html')
+
+@app.route('/api/NewInsurance', methods=['POST'])
+def api_new_insurance():
+    form_dict = {k: v for k, v in request.form.items()}
+
+    result = insurance.create(form_dict)
+    if(result):
+        message = f'New insurance created (id:{result})'
+    else:
+        message = 'Error encountered while attempting to create insurance!'
+
+    return redirect(url_for('efm_page', 
+                        message=message))
+
+# Delete insurance
+
+@app.route('/api/DeleteInsurance/<Ins_id>')
+def api_delete_insurance(Ins_id):
+    if(util.is_empty_string(Ins_id)):
+        return redirect(url_for('efm_page',
+                                message='Insurance could not be deleted, no Ins_id given'))
+    else:
+        result = ''
+        if insurance.delete(Ins_id):
+            result = f'Insurance deleted (id:{Ins_id})'
+        else:
+            result = f'Error encountered, could not delete insurance (id:{Ins_id})'
+        
+        return redirect(url_for('efm_page',
+                                message=result))
+
 
